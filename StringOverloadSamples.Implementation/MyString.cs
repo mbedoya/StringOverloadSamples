da6192da92ba68a7613472afb6e52735867102e1
mbedoya <mauricio.bedoya@gmail.com>
2017-04-23 10:07:18 -0500

    Initial Commit

---
+++ StringOverloadSamples.Implementation/MyString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringOverloadSamples.Implementation
+{
+    public class MyString
+    {
+        private string text;
+
+        public MyString(string text)
+        {
+            this.text = text;
+        }
+
+        public override string ToString()
+        {
+            return text;
+        }
+
+        public static MyString operator ++(MyString str)
+        {
+            if (String.IsNullOrEmpty(str.ToString()))
+            {
+                return new MyString(string.Empty);
+            }
+            return new MyString(str.text + str.text);
+        }
+    }
+}
---
+++ StringOverloadSamples.Test/StringOverloadTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringOverloadSamples.Implementation;
+
+namespace StringOverloadSamples.Test
+{
+    [TestClass]
+    public class StringOverloadTest
+    {
+        [TestMethod]
+        public void PlusPlusOperator_StringIsEmpty_EmptyResult()
+        {
+            MyString text = new MyString(string.Empty);
+            text++;
+            Assert.AreEqual(string.Empty, text.ToString());
+        }
+
+        [TestMethod]
+        public void PlusPlusOperator_StringIsNull_EmptyResult()
+        {
+            MyString text = new MyString(null);
+            text++;
+            Assert.AreEqual(string.Empty, text.ToString());
+        }
+
+        [TestMethod]
+        public void PlusPlusOperator_StringIsNotEmpty_StringDoubled()
+        {
+            MyString text = new MyString(" ");
+            text++;
+            Assert.AreEqual("  ", text.ToString());
+        }
+
+        [TestMethod]
+        public void PlusPlusOperator_StringHasChars_StringDoubled()
+        {
+            MyString text = new MyString("aa");
+            text++;
+            Assert.AreEqual("aaaa", text.ToString());
+        }
+    }
+}
